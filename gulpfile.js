a129c4ca9a482c91bb607c9cad970c5f6ce7ec51
Sebastian Schambaher <sebastianschambaher@gmail.com>
2021-10-28 18:47:32 -0500

    se disminuyó e tamaño de las imagenes

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,61 @@
-const { src, dest, watch } = require("gulp");
+const { src, dest, watch, parallel } = require("gulp");
+
+// CSS
 const sass = require("gulp-sass")(require("sass"));
 const plumber = require("gulp-plumber");
-const gulpWebp = require("gulp-webp");
 
-function convertToWebp () {
-    return src("./src/img/*.jpg")
-            .pipe(gulpWebp())
-            .pipe(dest("./build/img"))
+//Imagenes
+const cache = require('gulp-cache');
+const gulpWebp = require("gulp-webp");
+const imagemin = require('gulp-imagemin');
+const gulpAvif = require('gulp-avif');
+
+
+function convertToWebp (done) {
+    const opciones = {
+        quality: 50
+    }
+    src("src/img/**/*.{jpg,png}")
+        .pipe( gulpWebp(opciones) )
+        .pipe( dest("build/img") );
+
+    done();
 }
 
-function compileSass () {
-    return src("src/sass/**/*.scss")
-            .pipe(plumber())
-            .pipe(sass())
-            .pipe(dest("build/css"))
+function convertToAvif (done) {
+    const opciones = {
+        quality: 50
+    }
+    src("src/img/**/*.{jpg,png}")
+        .pipe( gulpAvif(opciones) )
+        .pipe( dest("build/img") );
+
+    done();
+}
+
+function convertToPng ( done ) {
+    const opciones = {
+        optimizationLevel : 3
+    }
+    src("src/img/**/*.{jpg,png}")
+        .pipe(cache(imagemin(opciones)))
+        .pipe(dest("build/img"));
+
+    done();
+}
+
+function compileSass (done) {
+    src("src/sass/**/*.scss")
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(dest("build/css"))
+    done();
 }
 
 function watchSass () {
     watch("./src/sass/**/*.scss", compileSass);
 }
 
-exports.webp = convertToWebp;
+exports.convertImage = parallel(convertToWebp, convertToPng, convertToAvif);
 exports.sass = compileSass;
 exports.default = watchSass;
